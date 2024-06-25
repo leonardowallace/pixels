2e1e625882176684f23d9419569765df94508e88
leonardowallace <leocoala551@gmail.com>
2024-06-25 13:55:08 -0300

    Criado carrossel e aba de tutoriais

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,23 @@ const blurHeader = () =>{
 }
 
 window.addEventListener('scroll', blurHeader)
+
+/*=============== SWIPER MAPAS ===============*/
+let swiperMaps = new Swiper('.maps__swiper', {
+    loop: true,
+    grabCursor: true,
+    slidesPerView: 2,
+    spaceBetween: 24,
+
+    breakpoints:{
+        440:{
+            slidesPerView: 'auto',
+        },
+        768:{
+            slidesPerView: 4,
+        },
+        1200:{
+            slidesPerView: 5,
+        }
+    }
+  });
