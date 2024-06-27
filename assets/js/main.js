d7a5ab6652e514ab58cfe2fa143f487969d432d3
leonardowallace <leocoala551@gmail.com>
2024-06-27 14:42:54 -0300

    ajuste de responsividade do menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,6 @@ const navMenu = document.getElementById('nav-menu'),
       navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
-/* Validate if constant exists */
 if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
@@ -14,7 +13,6 @@ if (navToggle) {
 }
 
 /*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
 if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
@@ -24,51 +22,16 @@ if (navClose) {
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link:not(.dropdown-toggle)')
+const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction() {
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link that is not a dropdown, we remove the show-menu class
     navMenu.classList.remove('show-menu')
     navClose.style.display = 'none'
     navToggle.style.display = 'block'
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
-
-/*=============== SHOW DROPDOWN MENU ===============*/
-const dropdownToggle = document.querySelectorAll('.dropdown-toggle')
-
-dropdownToggle.forEach(dt => {
-    dt.addEventListener('click', function (event) {
-        event.stopPropagation() // Prevent closing the main menu
-        const dropdownMenu = this.nextElementSibling
-        dropdownMenu.classList.toggle('show-dropdown')
-
-        // Close other open dropdowns
-        document.querySelectorAll('.nav__dropdown').forEach(dm => {
-            if (dm !== dropdownMenu) {
-                dm.classList.remove('show-dropdown')
-            }
-        })
-    })
-})
-
-/* Close dropdown menu when a dropdown link is clicked */
-const dropdownLinks = document.querySelectorAll('.nav__dropdown-link')
-
-dropdownLinks.forEach(dl => {
-    dl.addEventListener('click', function () {
-        const dropdownMenu = this.closest('.nav__dropdown')
-        dropdownMenu.classList.remove('show-dropdown')
-
-        // Optionally close the main menu if desired when clicking a dropdown link
-        const navMenu = document.getElementById('nav-menu')
-        navMenu.classList.remove('show-menu')
-        navClose.style.display = 'none'
-        navToggle.style.display = 'block'
-    })
-})
 
 /* Close dropdown menu when clicking outside */
 document.addEventListener('click', function (event) {
